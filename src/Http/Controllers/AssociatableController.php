313fd91eaf11d04486f70bf7c7a47f80a85cd10f
Marcus Dalgren <marcus@raket.co>
2019-05-08 17:28:03 +0200

    Fix associatable controller for group field

--- src/Http/Controllers/AssociatableController.php
+++ src/Http/Controllers/AssociatableController.php
@@ -2,8 +2,10 @@
 
 namespace R64\NovaFields\Http\Controllers;
 
+use Laravel\Nova\Fields\FieldCollection;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Http\Controllers\AssociatableController as Controller;
+use Yassi\NovaGroupField\NovaGroupField;
 
 class AssociatableController extends Controller
 {
@@ -15,15 +17,22 @@ class AssociatableController extends Controller
      */
     public function index(NovaRequest $request)
     {
-        $fields = $request->newResource()
-                        ->availableFields($request);
+	    $fields = $request->newResource()->availableFields($request);
+		$groupFieldsToMerge = new FieldCollection();
+	    foreach ( $fields as $field ) {
+		    if ($field instanceof NovaGroupField) {
+			    $groupFields = $field->meta()['fields'];
+			    $groupFieldsToMerge = $groupFieldsToMerge->merge($groupFields);
+		    }
+		}
 
+	    $fields = $fields->merge($groupFieldsToMerge);
         $field = $fields->firstWhere('attribute', $request->field);
 
         if(!$field) {
             $rowField = $fields->firstWhere('component', 'nova-fields-row');
-            $fields = collect($rowField->meta['fields']);
-            $field = $fields->firstWhere('attribute', $request->field);
+	        $fields = collect($rowField->meta['fields']);
+	        $field = $fields->firstWhere('attribute', $request->field);
         }
 
         $withTrashed = $this->shouldIncludeTrashed(
